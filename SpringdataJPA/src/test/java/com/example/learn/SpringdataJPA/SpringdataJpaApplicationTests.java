99f139e32c082f7e29c904897258b71581041353
Robiee <Robiee@DESKTOP-ITSRC0O>
2025-07-19 13:01:54 +0530

    Added Insert and show method in test class

--- SpringdataJPA/src/test/java/com/example/learn/SpringdataJPA/SpringdataJpaApplicationTests.java
+++ SpringdataJPA/src/test/java/com/example/learn/SpringdataJPA/SpringdataJpaApplicationTests.java
@@ -1,12 +1,14 @@
 package com.example.learn.SpringdataJPA;
 
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import com.example.learn.SpringdataJPA.Entity.Student;
-
-
+import com.example.learn.SpringdataJPA.Entity.StudentCollegeInfo;
 import com.example.learn.SpringdataJPA.repository.StudentRepository;
 
 
@@ -18,7 +20,43 @@ StudentRepository studentRepository;
 
 @Test
 public void saveStudents() {
-	Student student = Student.class.b ;
+	//Student student = Student;
+
+	StudentCollegeInfo collegeInfo = StudentCollegeInfo.builder().
+			passing_year("2K16").
+			percentage("9CGP").
+			school_name("MMU").
+			stream("MCA").build();
+	Student student = Student.builder().
+			city("Mumbai").
+			mail_ID("deva@gmail.com").
+			studentName("Deva mahajan").
+			collegeinfo(collegeInfo).build();
+		studentRepository.save(student);
+		System.out.println("added one record");
+		
+		StudentCollegeInfo collegeInfo1 = StudentCollegeInfo.builder().
+				passing_year("2K15").
+				percentage("8.5 CGP").
+				school_name("KMU").
+				stream("B.TECH").build();
+		Student student1 = Student.builder().
+				city("Kanpur").
+				mail_ID("niraj@gmail.com").
+				studentName("Niraj pandey").
+				collegeinfo(collegeInfo1).build();
+			studentRepository.save(student1);
+			System.out.println("added 2nd record");
+	
+	
+}
+
+@Test
+public void showStudent() {
+	
+	List<Student> students = studentRepository.findAll();
+	
+	System.out.println("fetched record from DB :" + students);
 	
 }
 	
